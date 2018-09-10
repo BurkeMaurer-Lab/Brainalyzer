04840330bef05d4d9e808db692639d9c1e65f928
guenther0623 <dylanguen@gmail.com>
2018-09-10 15:40:49 -0400

    Alpha Push
    Available for testing.

--- 01 - PreProcessing/Brain_PreProcess.m
+++ 01 - PreProcessing/Brain_PreProcess.m
@@ -2,7 +2,13 @@ function Brain_PreProcess(inDirTev, inDirSev, outDir, ratNum, blockID)
 
     clc;
     
+    chunkSize = 100;
     fill = 1; %Constant for if you want to try to fill in missing data points
+    
+    posX = [];
+    posY = [];
+    posTS = [];
+     
     blockID = char(blockID);
     %Necessary Constants
     % -cmPERpix
@@ -52,8 +58,17 @@ function Brain_PreProcess(inDirTev, inDirSev, outDir, ratNum, blockID)
                 inDir = inputDirS;
             end
             
-            eeg.(wavesBlock.wave(i).Name) = Brain_LoadWaveform(inDir, wavesBlock.wave(i), wavesBlock.wave(i).Type);
-            eeg.ts = [0 : 1/eeg.(wavesBlock.wave(i).Name).fs : (1/eeg.(wavesBlock.wave(i).Name).fs)*size(eeg.(wavesBlock.wave(i).Name).data, 2)];
+            dataInfo = TDT2mat(inputDirT, 'T1', 1, 'T2', 2, 'VERBOSE', 0);
+    
+            totalTime = str2double(dataInfo.info.duration(4:5))*60 ...
+                + str2double(dataInfo.info.duration(1:2))*60*60 ...
+                + str2double(dataInfo.info.duration(7:8));
+            
+            eeg.(wavesBlock.wave(i).Name) = Brain_LoadWaveform(inDir, wavesBlock.wave(i), wavesBlock.wave(i).Type, totalTime);
+            
+            eeg.ts = [0 ...
+                : 1/eeg.(wavesBlock.wave(i).Name).fs ...
+                : (1/eeg.(wavesBlock.wave(i).Name).fs)*(size(eeg.(wavesBlock.wave(i).Name).data, 2)-1)];
             %Cut out timeframes between epochs using timeVector
             %Need:
             %   Probe settings to generate prb files and organize channels by
@@ -75,41 +90,70 @@ function Brain_PreProcess(inDirTev, inDirSev, outDir, ratNum, blockID)
                 % Step 6: Delete unnecessary files
             end
             
-        
-            tempData = TDT2mat(inDirT, 'TYPE', {'scalars'}, 'T1', 0, 'T2', 0, 'VERBOSE', 0);
 
-            xposR = tempData.scalars.RVn1.data(3, :);
-            yposR = tempData.scalars.RVn1.data(4, :);
+
+            for T1 = 0:chunkSize:(totalTime-chunkSize)
+        
+                T2 = T1 + chunkSize;
+        
+                tempData = TDT2mat(inputDirT, 'TYPE', {'scalars'}, 'T1', T1, 'T2', T2, 'VERBOSE', 0);
     
-            xposG = tempData.scalars.RVn1.data(6, :);
-            yposG = tempData.scalars.RVn1.data(7, :);
-            posTS = tempData.saclars.RVn1.ts;
+                xposR = tempData.scalars.RVn1.data(3, :);
+                yposR = tempData.scalars.RVn1.data(4, :);
     
-            if fill
-                lostlocR = find(xposR == -1);
-                xposR(lostlocR) = NaN;
-                xposR = inpaintn(xposR);
-                yposR(lostlocR) = NaN;
-                yposR = inpaintn(yposR);
+                xposG = tempData.scalars.RVn1.data(6, :);
+                yposG = tempData.scalars.RVn1.data(7, :);
+                posTS = [posTS, tempData.scalars.RVn1.ts];
     
-                lostlocG = find(xposG == -1);
-                xposG(lostlocG) = NaN;
-                xposG = inpaintn(xposG);
-                yposG(lostlocG) = NaN;
-                yposG = inpaintn(yposG);
+                if fill
+                    lostlocR = find(xposR == -1);
+                    xposR(lostlocR) = NaN;
+                    xposR = inpaintn(xposR);
+                    yposR(lostlocR) = NaN;
+                    yposR = inpaintn(yposR);
+    
+                    lostlocG = find(xposG == -1);
+                    xposG(lostlocG) = NaN;
+                    xposG = inpaintn(xposG);
+                    yposG(lostlocG) = NaN;
+                    yposG = inpaintn(yposG);
+                end
+            
+                posX = [posX, xposR];
+                posY = [posY, yposR];
+            
             end
-            
-            posX = xposR;
-            posY = yposR;
-            
-            temp_ts = mean([posTS(2:end); posTS(1:end-1)], 1);
-            interp = interp1([eeg.ts(1) temp_ts eeg.ts(end)], [eeg.ts(1) temp_ts eeg.ts(end)], eeg.ts, 'spline');
+    
+            %temp_ts = mean([posTS(2:end); posTS(1:end-1)], 1);
+            interp = interp1([eeg.ts(1) posTS eeg.ts(end)], [eeg.ts(1) posTS eeg.ts(end)], eeg.ts, 'spline');
         
             eeg.pos(1, :) = interp1(posTS, posX, interp, 'spline');
             eeg.pos(2, :) = interp1(posTS, posY, interp, 'spline');
             
         end
+        
+
+        
+        
     end
+    
+    for j = 1:size(timeVector, 2)
+        strt = find(eeg.ts >= timeVector(j, 1), 'FIRST');
+        endr = find(eeg.ts >= timeVector(j, 2), 'FIRST');
+            
+        label = strcat(outputDir, 'Epoch', sprintf('%02d', j));
+            
+        epoch.ts = eeg.ts(strt:endr);
+        epoch.pos = eeg.pos(strt:endr);
+        for k = 1:size(wavesBlock.waves, 2)
+            epoch.(wavesBlock.wave(k).Name).data = eeg.(wavesBlock.wave(k).Name).data(strt:endr);
+            epoch.(wavesBlock.wave(k).Name).fs = eeg.(wavesBlock.wave(k).Name).fs;
+        end
+        
+        save(label, -struct', 'epoch', '-v7.3');
+    end
+    
+    %%save
     
     
     
@@ -131,9 +175,10 @@ function Brain_PreProcess(inDirTev, inDirSev, outDir, ratNum, blockID)
         
 end
 
-function eegTemp = Brain_LoadWaveform(inDir, wave, dataType)
+function eegTemp = Brain_LoadWaveform(inDir, wave, dataType, totalTime)
 
     eegTemp.data = [];
+    chunkSize = 500;
     
     %wave = waveInfo.wave;
     %clear waveInfo;
@@ -144,26 +189,47 @@ function eegTemp = Brain_LoadWaveform(inDir, wave, dataType)
         cprintf('text', ['Loading Channel ', sprintf('%02d', i), '\n']);
         
         if strcmp(dataType, 'tev')
-            dataTemp = TDT2mat(inDir, 'CHANNEL', i, 'TYPE', {'streams'}, 'STORE', {wave.waveID}, ...
-                'VERBOSE', 0);
-        %'T1', 0, 'T2', 0, 'VERBOSE', 0);
-            fs = floor(dataTemp.streams.(wave.waveID).fs);
-            if wave.RecoFreq == (floor(fs/1000))
-                gap = fs / wave.SaveFreq;
-            else
-                cprintf('*err', 'ERROR:\n');
-                cprintf('err', 'Frequency in template and recorded frequency in raw files do not match\n');
+            dataHolder = [];
+            for T1 = 0:chunkSize:totalTime
+                
+                T2 = T1 + chunkSize;
+                if T2 > totalTime
+                    T2 = totalTime;
+                end
+                
+                if T2 == T1
+                    break;
+                end
+                
+                dataTemp = TDT2mat(inDir, 'CHANNEL', i, 'TYPE', {'streams'}, 'STORE', {wave.waveID}, ...
+                    'T1', T1, 'T2', T2, 'VERBOSE', 0);
+                
+                fs = floor(dataTemp.streams.(wave.waveID).fs);
+        
+                if wave.RecoFreq == (floor(fs/1000))
+                    gap = floor((fs / wave.SaveFreq)/1000);
+                else
+                    cprintf('*err', 'ERROR:\n');
+                    cprintf('err', 'Frequency in template and recorded frequency in raw files do not match\n');
+                end
+                
+                %if isempty(dataHolder)
+                %    dataHolder = zeros(1, (dataTemp.streams.(wave.waveID).fs*totalTime));
+                %end
+                
+                dataHolder = [dataHolder, dataTemp.streams.(wave.waveID).data(1, 1:gap:end)];
+            
+                
             end
             
-            eegTemp.data(i, :) = dataTemp(1, 1:gap:end);
-            
-            clear dataTemp;
+            eegTemp.data(i, :) = dataHolder; %dataTemp.streams.(wave.waveID).data(1, 1:gap:end);
+            clear dataHolder;
             
         elseif strcmp(dataType, 'sev')
             dataTemp = SEV2mat(inDir, 'CHANNEL', i, 'VERBOSE', 0);
             fs = floor(dataTemp.(wave.waveID).fs);
             if wave.RecoFreq == (floor(fs/1000))
-                gap = fs / wave.SaveFreq;
+                gap = floor((fs / wave.SaveFreq)/1000);
             else
                 cprintf('*err', 'ERROR:\n');
                 cprintf('err', 'Frequency in template and recorded frequency in raw files do not match\n');
@@ -176,4 +242,3 @@ function eegTemp = Brain_LoadWaveform(inDir, wave, dataType)
     end
     eegTemp.fs = fs / gap;
 end
-    
