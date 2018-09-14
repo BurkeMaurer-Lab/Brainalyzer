18e527b93cd8943e7b52f123f52f1989778cdf7d
ndicola <ndicola3@gmail.com>
2018-09-14 13:18:47 -0400

    PrintPercentage Fix 2
    Deleted unneeded code from the PrintPercentage function

--- 00 - Interface/printPercentage.m
+++ 00 - Interface/printPercentage.m
@@ -5,19 +5,5 @@ function outMsg = printPercentage(done, toDo, prevMsg)
     pctDone = floor(pctDone);
     outMsg = strcat(sprintf('%g', pctDone), '.', sprintf('%g', md), '%% Complete');
     cprintf('text', [repmat('\b', 1, (length(prevMsg) - (1 * ~isempty(prevMsg)))), outMsg]);
-%     fprintf('\n\n')
-%     disp(length(prevMsg))
-%     disp(length(outMsg))
-%     disp(outMsg)
-%     fprintf('\n')
-%     fprintf(outMsg)
-%     error
-    
-%     if pctDone >= 100
-%         cprintf('text', ['\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b', outMsg]);
-%     elseif pctDone >= 10
-%         cprintf('text', ['\b\b\b\b\b\b\b\b\b\b\b\b\b\b', outMsg]);
-%     else
-%         cprintf('text', ['\b\b\b\b\b\b\b\b\b\b\b\b\b', outMsg]);
-%     end
+
 end
